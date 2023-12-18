6402d42b1873d4d5ae7e252468a6e788114df0d7
wangming <wangming@kanzhun.com>
2023-12-18 18:12:41 +0800

    feat: 添加chatgpt

--- README.md
+++ README.md
@@ -4,17 +4,21 @@
 
 项目中，.vscode 目录，右键 create snippet，在 maker 平台查找模版 id
 
-## 代码添加注释
+## 一、代码添加注释
 
 选中代码，执行 cmd+t，或者 shift+cmd+p,输入 chatgpt comment
 
-## 生成代码
+## 二、生成代码
 
 输入注释，执行 cmd+g, 或者 shift+cmd+p,输入 chatgpt generate
 
-## 代码优化
+## 三、代码优化
 
 选中代码，执行 cmd+o, 或者 shift+cmd+p,输入 chatgpt optimization
+
+## 四、查询内容
+
+点击左侧导航，可以在线查询内容。
 
 ## 注意
 
--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 import * as vscode from 'vscode';
 import { createSnippet, saveSnippet } from './command/createSnippet';
 import { chatgpt, generateCode, optimizationCode } from './command/chatgpt';
+import { ChatWebview } from './modules/chatgpt/navbar/searchText';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -29,6 +30,29 @@ export function activate(context: vscode.ExtensionContext) {
     });
     const disposable6 = vscode.commands.registerCommand('chatgpt.optimization', () => {
         optimizationCode();
+    });
+
+    const chatWebview = new ChatWebview();
+    vscode.window.registerWebviewViewProvider('Chat-sidebar', chatWebview, {
+        webviewOptions: {
+            // 这是一个比较有用的配置项，可以确保你的插件在不可见时不会被销毁，建议开启，否侧每次打开都会重新加载一次插件
+            retainContextWhenHidden: true,
+        },
+    });
+    // 这里实现了一个简单的功能，在vscode打开的文件中，选中代码时会实时展示在web页面上
+    // 监听用户选中文本事件
+    vscode.window.onDidChangeTextEditorSelection((event) => {
+        const editor = event.textEditor;
+        let document = editor.document;
+        let selection = editor.selection;
+        // 获取当前窗口的文本
+        let text = document.getText(selection);
+        // 上文提到chatWebview可能为null 因此需要可选链写法，所以这里存在不稳定性，不过测试没问题~
+        chatWebview?.webview?.webview.postMessage({
+            // 第一次postMessage，下一次在chatWebview文件的iframe中
+            command: 'vscodeSendMesToWeb',
+            data: text,
+        });
     });
 
     context.subscriptions.push(disposable2, disposable3, disposable4, disposable5, disposable6);
